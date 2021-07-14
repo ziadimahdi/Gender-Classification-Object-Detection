e3dad7d0dca0094b45134cdb7c4848ecc2d8034e
ZIADI MAHDI <56120825+ziadimahdi@users.noreply.github.com>
2021-07-14 22:09:17 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"ziadi.isetn@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
